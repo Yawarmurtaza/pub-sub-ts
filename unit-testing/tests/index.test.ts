0bfe3e0a89ecbbfe57963a4a71ec141ae279992f
Yawarmurtaza <yakhuwaj@microsoft.com>
2023-03-02 19:39:13 -0800

    commited everything

--- unit-testing/tests/index.test.ts
+++ unit-testing/tests/index.test.ts
@@ -4,4 +4,9 @@ describe('testing index file', () => {
   test('empty string should result in zero', () => {
     expect(add('')).toBe(0);
   });
+
+  test('empty string should result in zero', () => {
+    expect(add('1,2,3,4,5')).toBe(15);
+  });
+
 });
