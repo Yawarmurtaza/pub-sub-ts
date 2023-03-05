d5c62dff7aef8267e3fb53391d5e4a4cc5eec4d8
Yawar Khuwaja <yakhuwaj@microsoft.com>
2023-03-05 07:27:15 -0800

    LRU added - unit tests are passing

--- unit-testing/LRU.ts
+++ unit-testing/LRU.ts
@@ -1,15 +1,92 @@
 export class Lru {
-    constructor() { }
 
-    public Add(key: string, value: string): void {
-        throw new Error("Not implemented");
+    private head: DoubleyLinkedList<string, string>;
+    private tail: DoubleyLinkedList<string, string>;
+    private map: Map<string, DoubleyLinkedList<string, string>>;
+
+    constructor(private capacity: number = 5) {
+        this.map = new Map<string, DoubleyLinkedList<string, string>>()
+        this.head = new DoubleyLinkedList<string, string>("head", "head");
+        this.tail = new DoubleyLinkedList<string, string>("tail","tail");
+
+        this.head.Next = this.tail;
+        this.tail.Previous = this.head;
+    }
+
+    public AddOrUpdate(key: string, value: string): boolean {
+        
+        const node = this.map.get(key);
+        if (node) {
+            // update existing value
+            this.RemoveNode(node);
+            node.Value = value; // update node with new value
+            this.MoveNodeToHead(node); // move node to head        
+
+        } else {
+            // add new value
+            if (this.map.size >= this.capacity) {
+                // remove the previous node from the tail
+                const tail = this.tail.Previous!;
+                this.RemoveNode(tail.Previous!);
+                this.map.delete(tail.Key);
+            }
+
+            // now add new node to the head
+            const newNode = new DoubleyLinkedList<string, string>(key ,value);
+            this.MoveNodeToHead(newNode);
+            this.map.set(key, newNode);
+        }
+
+        return true;
+    }
+
+    public Remove(key: string): boolean {
+        const node = this.map.get(key);
+        if (node) {
+            this.RemoveNode(node);
+            this.map.delete(key);
+            return true;
+        }
+        return false;
     }
 
     public Get(key: string): string | undefined {
-        throw new Error("Not implemented");
+        const node = this.map.get(key);
+        if (node) {
+            // remove this node from its current position
+            this.RemoveNode(node);
+
+            // add this node to the head
+            this.MoveNodeToHead(node);    
+            return node.Value;               // return the value
+        }
+        return undefined;
     }
 
     public Exists(key: string): boolean {
         throw new Error("Not implemented");
     }
+
+    private RemoveNode(node: DoubleyLinkedList<string, string>): void {
+        node.Previous!.Next = node.Next;
+        node.Next!.Previous = node.Previous;
+    }
+
+    private MoveNodeToHead(node: DoubleyLinkedList<string, string>): void {
+        this.head.Next!.Previous = node; // 2nd node from head is reverse pointing to node
+        node.Next = this.head.Next;      // node is forward pointing to 2nd node from head
+        node.Previous = this.head;       // node is reverse pointing to head
+        this.head.Next = node;           // head is forward pointing to node    
+    }
+
+
+}
+
+class DoubleyLinkedList<TKey, TValue> {
+    constructor(public Key: TKey, public Value: TValue) {
+
+    }
+
+    public Next: DoubleyLinkedList<TKey, TValue> | undefined;
+    public Previous: DoubleyLinkedList<TKey, TValue> | undefined;
 }
--- unit-testing/tests/LRU.test.ts
+++ unit-testing/tests/LRU.test.ts
@@ -1,16 +1,98 @@
 import { Lru } from "../LRU";
 
 describe("LRU Test Suite", () => {
-    test("Add should add key value pair", () => { 
-        // Arrange
-        const lru = new Lru();
-    });
-
     
     test("Get should return existing data using key", () => { 
-        // Arrange
-        const lru = new Lru();
+         // Arrange
+         const lru = new Lru(3);
+         lru.AddOrUpdate("key1", "value1");
+         lru.AddOrUpdate("key2", "value2");
+         lru.AddOrUpdate("key3", "value3");
+         
+         // Act
+         const result = lru.Get("key1");
+ 
+         // Assert
+         expect(result).toBe("value1");
+    });
+
+    test ("AddOrUpdate should add new data", () => {
+         // Arrange
+         const lru = new Lru(3);
+         lru.AddOrUpdate("key1", "value1");
+         const result = lru.Get("key1");
+         
+         lru.AddOrUpdate("key1", "new value");
+         
+         // Act
+         const newResult = lru.Get("key1");
+ 
+         // Assert
+         expect(result).toBe("value1");
+         expect(newResult).toBe("new value");
+    });
+
+    test("It should remove the least recently used item", () => {
+        const lru = new Lru(3);
+        lru.AddOrUpdate("key1", "value1");
+        lru.AddOrUpdate("key2", "value2");
+        lru.AddOrUpdate("key3", "value3");
+        lru.AddOrUpdate("key4", "value4");
+
+        const result = lru.Get("key1");
+
+        expect(result).toBeUndefined();
     });
 
 
+    test("It should remove the least recently used item - 2", () => {
+        const lru = new Lru();
+        lru.AddOrUpdate("key1", "value1");
+        lru.AddOrUpdate("key2", "value2");
+        lru.AddOrUpdate("key3", "value3");
+        lru.AddOrUpdate("key4", "value4");
+        lru.AddOrUpdate("key5", "value5");
+        //lru.AddOrUpdate("key6", "value6");
+
+        lru.Get("key1");
+        lru.Get("key2");
+
+        lru.AddOrUpdate("key6", "value6");
+
+        const result = lru.Get("key3");
+
+        expect(result).toBeUndefined();
+    });
+
+    test("It should remove the key", () => {
+        // Arrange.
+        const lru = new Lru(3);
+        lru.AddOrUpdate("key1", "value1");
+        lru.AddOrUpdate("key2", "value2");
+        
+        // Act.
+        const result = lru.Remove("key1");
+
+        // Assert.
+        expect(result).toBeTruthy();
+        expect(lru.Get("key1")).toBeUndefined();
+        
+    });
+
+    test("It should return undefined when key is not found", () => {
+        
+        // Arrange
+        const lru = new Lru(3);
+        lru.AddOrUpdate("key1", "value1");
+        lru.AddOrUpdate("key2", "value2");
+        lru.AddOrUpdate("key3", "value3");
+        lru.AddOrUpdate("key4", "value4");
+
+        // Act.
+        const result = lru.Get("key5");
+
+        // Assert
+        expect(result).toBeUndefined();
+
+    });
 });
