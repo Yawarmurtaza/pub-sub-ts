bda44d5df96d41fd1bd337101316becfc4420c26
Yawar Khuwaja <yakhuwaj@microsoft.com>
2023-03-04 23:56:09 -0800

    LRU in progress

---
+++ unit-testing/LRU.ts
@@ -0,0 +1,15 @@
+export class Lru {
+    constructor() { }
+
+    public Add(key: string, value: string): void {
+        throw new Error("Not implemented");
+    }
+
+    public Get(key: string): string | undefined {
+        throw new Error("Not implemented");
+    }
+
+    public Exists(key: string): boolean {
+        throw new Error("Not implemented");
+    }
+}
---
+++ unit-testing/tests/LRU.test.ts
@@ -0,0 +1,16 @@
+import { Lru } from "../LRU";
+
+describe("LRU Test Suite", () => {
+    test("Add should add key value pair", () => { 
+        // Arrange
+        const lru = new Lru();
+    });
+
+    
+    test("Get should return existing data using key", () => { 
+        // Arrange
+        const lru = new Lru();
+    });
+
+
+});
