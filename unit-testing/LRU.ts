63a3be54fc28387b8db95b821e4ee2369ace1a47
Yawar Khuwaja <yakhuwaj@microsoft.com>
2023-03-05 15:39:15 -0800

    LRU is generic

--- unit-testing/LRU.ts
+++ unit-testing/LRU.ts
@@ -1,19 +1,19 @@
-export class Lru {
+export class Lru<TKey, TValue> {
 
-    private head: DoubleyLinkedList<string, string>;
-    private tail: DoubleyLinkedList<string, string>;
-    private map: Map<string, DoubleyLinkedList<string, string>>;
+    private head: DoubleyLinkedList<TKey, TValue>;
+    private tail: DoubleyLinkedList<TKey, TValue>;
+    private map: Map<TKey, DoubleyLinkedList<TKey, TValue>>;
 
     constructor(private capacity: number = 5) {
-        this.map = new Map<string, DoubleyLinkedList<string, string>>()
-        this.head = new DoubleyLinkedList<string, string>("head", "head");
-        this.tail = new DoubleyLinkedList<string, string>("tail","tail");
+        this.map = new Map<TKey, DoubleyLinkedList<TKey, TValue>>()
+        this.head = new DoubleyLinkedList<TKey, TValue>();
+        this.tail = new DoubleyLinkedList<TKey, TValue>();
 
         this.head.Next = this.tail;
         this.tail.Previous = this.head;
     }
 
-    public AddOrUpdate(key: string, value: string): boolean {
+    public AddOrUpdate(key: TKey, value: TValue): boolean {
         
         const node = this.map.get(key);
         if (node) {
@@ -28,11 +28,11 @@ export class Lru {
                 // remove the previous node from the tail
                 const tail = this.tail.Previous!;
                 this.RemoveNode(tail.Previous!);
-                this.map.delete(tail.Key);
+                this.map.delete(tail.Key!);
             }
 
             // now add new node to the head
-            const newNode = new DoubleyLinkedList<string, string>(key ,value);
+            const newNode = new DoubleyLinkedList<TKey, TValue>(key ,value);
             this.MoveNodeToHead(newNode);
             this.map.set(key, newNode);
         }
@@ -40,7 +40,7 @@ export class Lru {
         return true;
     }
 
-    public Remove(key: string): boolean {
+    public Remove(key: TKey): boolean {
         const node = this.map.get(key);
         if (node) {
             this.RemoveNode(node);
@@ -50,7 +50,7 @@ export class Lru {
         return false;
     }
 
-    public Get(key: string): string | undefined {
+    public Get(key: TKey): TValue | undefined {
         const node = this.map.get(key);
         if (node) {
             // remove this node from its current position
@@ -63,16 +63,16 @@ export class Lru {
         return undefined;
     }
 
-    public Exists(key: string): boolean {
+    public Exists(key: TKey): boolean {
         throw new Error("Not implemented");
     }
 
-    private RemoveNode(node: DoubleyLinkedList<string, string>): void {
+    private RemoveNode(node: DoubleyLinkedList<TKey, TValue>): void {
         node.Previous!.Next = node.Next;
         node.Next!.Previous = node.Previous;
     }
 
-    private MoveNodeToHead(node: DoubleyLinkedList<string, string>): void {
+    private MoveNodeToHead(node: DoubleyLinkedList<TKey, TValue>): void {
         this.head.Next!.Previous = node; // 2nd node from head is reverse pointing to node
         node.Next = this.head.Next;      // node is forward pointing to 2nd node from head
         node.Previous = this.head;       // node is reverse pointing to head
@@ -82,8 +82,8 @@ export class Lru {
 
 }
 
-class DoubleyLinkedList<TKey, TValue> {
-    constructor(public Key: TKey, public Value: TValue) {
+class DoubleyLinkedList<TKey = any, TValue = any> {
+    constructor(public Key?: TKey, public Value?: TValue) {
 
     }
 
